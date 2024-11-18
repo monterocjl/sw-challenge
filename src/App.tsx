b68ad887c8aa2b7b94519e2e6e5a499ff8d6d6e0
jmontero-sanna <juan.montero@qdsgroup.com.pe>
2024-11-18 12:40:44 -0500

    feature: Responsive Layout added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Layout } from "./components";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<div>Home page</div>} />
-        <Route path='/people' element={<div>People page</div>} />
-        <Route path='/planets' element={<div>Planets page</div>} />
-        <Route path='/starships' element={<div>Starships page</div>} />
-        <Route path='/personas/:id' element={<div>Person page</div>} />
-        <Route path='/planets/:id' element={<div>Planet page</div>} />
-        <Route path='/starship/:id' element={<div>Starship page</div>} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<div>Home page</div>} />
+          <Route path='/people' element={<div>People page</div>} />
+          <Route path='/planets' element={<div>Planets page</div>} />
+          <Route path='/starships' element={<div>Starships page</div>} />
+          <Route path='/personas/:id' element={<div>Person page</div>} />
+          <Route path='/planets/:id' element={<div>Planet page</div>} />
+          <Route path='/starship/:id' element={<div>Starship page</div>} />
+        </Route>
       </Routes>
     </Router>
   );
